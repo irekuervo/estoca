1c314b1c3b821c0c48155a3a532aee0750af4653
eschatz <eschatz@ispectrum.com.ar>
2023-10-29 19:31:07 -0300

    mejoras

--- Tp2/ejercicio_4.m
+++ Tp2/ejercicio_4.m
@@ -1,93 +1,146 @@
 % Ejercicio 4
 
+% Parametros
+tiempo_ventana = 50e-3;
+P = 30;
+
 %% audio_01
 close all;
 
-% Parametros
-tiempo_ventana = 50e-3;
-P = 50;
 alfa = 0.09;
 
 [audio, audio_regenerado, samplerate, reCorrs] = LPC("audio_01.wav", tiempo_ventana, P, alfa);
 
-% Graficamos normalizado
-figure
-plot(audio./max(audio))
+muestras = size(audio);
+
+% Graficamos normalizado en energia
+graf = figure;
+t = linspace(1, muestras(1) / samplerate, muestras(1));
+plot(t, audio./rms(audio));
 hold on
-plot(audio_regenerado./max(audio_regenerado))
+grid on;
+plot(t, audio_regenerado./rms(audio_regenerado));
+title('Comparación de audios')
+xlim([t(1) t(end)])
+xlabel('Tiempo (s)'), ylabel('Amplitud normalizada')
+legend('Audio Original','Audio Reconstruido')
+saveas(graf,"ej4_1_1.png")
 
 % Vemos los residuos para calibrar el alfa
-figure
-plot(reCorrs);
+graf = figure;
+x = 1: samplerate / muestras(1);
+plot( reCorrs);
+grid on;
+title('Autocorrelaciónes del residuo de la ventana')
+xlabel('Frecuencia (Hz)'), ylabel('Amplitud normalizada')
+saveas(graf,"ej4_1_2.png")
 
+% Reproducimos el audio regenerado
 player = audioplayer(audio_regenerado, samplerate);
 playblocking(player);
 
 %% audio_02
 close all;
 
-% Parametros
-tiempo_ventana = 50e-3;
-P = 50;
-alfa = 0.07;
+alfa = 0.06;
 
 [audio, audio_regenerado, samplerate, reCorrs] = LPC("audio_02.wav", tiempo_ventana, P, alfa);
 
-% Graficamos normalizado
-figure
-plot(audio./max(audio))
+muestras = size(audio);
+
+% Graficamos normalizado en energia
+graf = figure;
+t = linspace(1, muestras(1) / samplerate, muestras(1));
+plot(t, audio./rms(audio));
 hold on
-plot(audio_regenerado./max(audio_regenerado))
+grid on;
+plot(t, audio_regenerado./rms(audio_regenerado));
+title('Comparación de audios')
+xlim([t(1) t(end)])
+xlabel('Tiempo (s)'), ylabel('Amplitud normalizada')
+legend('Audio Original','Audio Reconstruido')
+saveas(graf,"ej4_2_1.png")
 
 % Vemos los residuos para calibrar el alfa
-figure
-plot(reCorrs);
+graf = figure;
+x = 1: samplerate / muestras(1);
+plot( reCorrs);
+grid on;
+title('Autocorrelaciónes del residuo de la ventana')
+xlabel('Frecuencia (Hz)'), ylabel('Amplitud normalizada')
+saveas(graf,"ej4_2_2.png")
 
+% Reproducimos el audio regenerado
 player = audioplayer(audio_regenerado, samplerate);
 playblocking(player);
 
 %% audio_03
 close all;
 
-% Parametros
-tiempo_ventana = 50e-3;
-P = 50;
 alfa = 0.1;
 
 [audio, audio_regenerado, samplerate, reCorrs] = LPC("audio_03.wav", tiempo_ventana, P, alfa);
 
-% Graficamos normalizado
-figure
-plot(audio./max(audio))
+muestras = size(audio);
+
+% Graficamos normalizado en energia
+graf = figure;
+t = linspace(1, muestras(1) / samplerate, muestras(1));
+plot(t, audio./rms(audio));
 hold on
-plot(audio_regenerado./max(audio_regenerado))
+grid on;
+plot(t, audio_regenerado./rms(audio_regenerado));
+title('Comparación de audios')
+xlim([t(1) t(end)])
+xlabel('Tiempo (s)'), ylabel('Amplitud normalizada')
+legend('Audio Original','Audio Reconstruido')
+saveas(graf,"ej4_3_1.png")
 
 % Vemos los residuos para calibrar el alfa
-figure
-plot(reCorrs);
+graf = figure;
+x = 1: samplerate / muestras(1);
+plot( reCorrs);
+grid on;
+title('Autocorrelaciónes del residuo de la ventana')
+xlabel('Frecuencia (Hz)'), ylabel('Amplitud normalizada')
+saveas(graf,"ej4_3_2.png")
 
+% Reproducimos el audio regenerado
 player = audioplayer(audio_regenerado, samplerate);
 playblocking(player);
+
 
 %% audio_04
 close all;
 
-% Parametros
-tiempo_ventana = 50e-3;
-P = 50;
-alfa = 0.1;
+alfa = 0.05;
 
 [audio, audio_regenerado, samplerate, reCorrs] = LPC("audio_04.wav", tiempo_ventana, P, alfa);
 
-% Graficamos normalizado
-figure
-plot(audio./max(audio))
+muestras = size(audio);
+
+% Graficamos normalizado en energia
+graf = figure;
+t = linspace(1, muestras(1) / samplerate, muestras(1));
+plot(t, audio./rms(audio));
 hold on
-plot(audio_regenerado./max(audio_regenerado))
+grid on;
+plot(t, audio_regenerado./rms(audio_regenerado));
+title('Comparación de audios')
+xlim([t(1) t(end)])
+xlabel('Tiempo (s)'), ylabel('Amplitud normalizada')
+legend('Audio Original','Audio Reconstruido')
+saveas(graf,"ej4_4_1.png")
 
 % Vemos los residuos para calibrar el alfa
-figure
+graf = figure;
+x = 1: samplerate / muestras(1);
 plot(reCorrs);
+grid on;
+title('Autocorrelaciónes del residuo de la ventana')
+xlabel('Frecuencia (Hz)'), ylabel('Amplitud normalizada')
+saveas(graf,"ej4_4_2.png")
 
+% Reproducimos el audio regenerado
 player = audioplayer(audio_regenerado, samplerate);
 playblocking(player);
