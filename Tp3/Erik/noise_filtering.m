320e171fad51091672a2e0efa303a97bd89aa8a6
eschatz <eschatz@ispectrum.com.ar>
2023-12-02 21:15:21 -0300

    .

--- Tp3/Erik/noise_filtering.m
+++ Tp3/Erik/noise_filtering.m
@@ -1,8 +1,5 @@
 function [W, V_est, J_est, Err_est] = noise_filtering(S, V, U, mu, w0)
 
-S = flip(S);
-V = flip(V);
-U = flip(U);
 X = S + V;
 
 [N,L] = size(X);
@@ -13,21 +10,21 @@ V_est = zeros(N,1);
 J_est = zeros(N,1);
 Err_est = zeros(N,1);
 
-for k = 1 : N - 1
-    indice_M = k + M - 1;
+for i = 1 : N - 1
+    indice_M = i + M - 1;
     
     if indice_M <= N
-        V_est(k) =  U(k:indice_M)'*W(:,k); % lo pasamos por el filtro actual
-        error = X(k) - V_est(k);
-        W(:,k+1) = W(:,k) + mu * U(k:indice_M) * error; % corregimos el filtro
+        V_est(i) =  U(i:indice_M)'*W(:,i); % lo pasamos por el filtro actual
+        error = X(i) - V_est(i);
+        W(:,i+1) = W(:,i) + mu * U(i:indice_M) * error; % corregimos el filtro
     else 
-        W(:,k+1) = W(:,k); % los ultimos directamente los copio para no hacer zero padding
+        W(:,i+1) = W(:,i); % los ultimos directamente los copio para no hacer zero padding
     end
    
     % Calculo de aprendizaje y error
-    S_est = X(k) - V_est(k);
-    Err_est(k) = ((S_est - S(k))^2);
-    J_est(k) = ((V_est(k) - V(k))^2);
+    S_est = X(i) - V_est(i);
+    Err_est(i) = ((S_est - S(i))^2);
+    J_est(i) = ((V_est(i) - V(i))^2);
 end
 
 end
